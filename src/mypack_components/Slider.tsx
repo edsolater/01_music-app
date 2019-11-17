63c04f116e821e43230c37ccabd53efe2b54ef5c
edsolater <edsolater@qq.com>
2019-11-17 16:37:49 +0800

    Slider 可以滑动且运算速度足够快

--- src/mypack_components/Slider.tsx
+++ src/mypack_components/Slider.tsx
@@ -1,16 +1,40 @@
-import React, { FC } from 'react'
+import React, { FC, useRef, useState } from 'react'
 import * as classnames from 'classnames'
 import { ClassValue } from 'classnames/types'
+
 const Slider: FC<{
   /**
    * 接收classnames()能接收的各种参数
    */
   className?: ClassValue
-} & JSX.IntrinsicElements['div']> = ({ className, ...restProps }) => (
-  <div className={classnames(className, 'Slider')} {...restProps}>
-    <div className="SliderTrack" />
-    <div className="SliderTrigger" />
-  </div>
-)
+} & JSX.IntrinsicElements['div']> = ({ className, ...restProps }) => {
+  const sliderTriggerRef = useRef(null)
+  const [pasedLeft, setLeft] = useState(0)
+  const dragTrigger = (event: MouseEvent, parentNode:HTMLElement) => {
+    const newLeft = event.clientX -  parentNode.offsetLeft
+    setLeft(newLeft)
+  }
+  return (
+    <div className={classnames(className, 'Slider')} {...restProps}>
+      <div
+        className="SliderTrigger"
+        ref={sliderTriggerRef}
+        onMouseDown={() => {
+          const parentElement = (((sliderTriggerRef.current as unknown) as HTMLDivElement).parentElement)!
+          const tempMoveTrigger = (e: MouseEvent) => dragTrigger(e, parentElement)
+          const tempSolidTrigger = () => {
+            document.removeEventListener('mousemove', tempMoveTrigger)
+            document.removeEventListener('mouseup', tempSolidTrigger)
+          }
+          document.addEventListener('mousemove', tempMoveTrigger)
+          document.addEventListener('mouseup', tempSolidTrigger)
+        }}
+        style={{
+          left: pasedLeft
+        }}
+      />
+      <div className="SliderTrack" />
+    </div>
+  )
+}
 export default Slider
-
