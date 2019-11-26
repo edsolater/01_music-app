c9f0f6ba97f5708ddcf962ad9e72079e1d41d164
edsolater <edsolater@qq.com>
2019-11-26 13:43:09 +0800

    上抛commander与reporter

--- src/mypack/components/Popover/Popover.tsx
+++ src/mypack/components/Popover/Popover.tsx
@@ -3,13 +3,21 @@ import * as classnames from 'classnames'
 import { ClassValue } from 'classnames/types'
 import './Popover.less'
 import { useUICommander } from '../__customHooks'
-
+type Commander = {
+  showPopover(): void
+}
+type Reporter = {
+  isOn: boolean
+}
 function Popover({
   className,
   isOn: _isOn,
   delayTime,
   Content,
+
   children,
+  commander,
+  reporter,
   ...restProps
 }: {
   /**
@@ -24,9 +32,37 @@ function Popover({
    * 延迟关闭的延迟时间。(只有当交互行为由组件内部自行控制控时生效)
    */
   delayTime?: number
+  /**
+   * Popover 的content的内容
+   */
   Content?: ReactNode
+  /**
+   * 暴露出Popover组件的控制命令
+   */
+  commander?: Commander
+  /**
+   * 暴露出Popover组件的当前状态
+   */
+  reporter?: Reporter
 } & JSX.IntrinsicElements['div']) {
   const controller = useUICommander({ type: 'open-close' }).isImmutable(_isOn)
+
+  //#region 上抛commander
+  if (commander) {
+    Object.assign(commander, {
+      showPopover() {
+        return controller.show()
+      },
+    } as Commander)
+  }
+  //#endregion
+  //#region 上抛reporter
+  if (reporter) {
+    Object.assign(reporter, {
+      isOn: _isOn ?? controller.isOn,
+    } as Reporter)
+  }
+  //#endregion
   return (
     <div
       className={classnames(className, 'Popover', 'Wrapper', { on: _isOn ?? controller.isOn })}
