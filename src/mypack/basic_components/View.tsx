5c1d2cd9d01b7a30a3922510bc85acae690b1130
zhanggongying <edsolater@qq.com>
2020-01-16 16:32:12 +0800

    把View打造成能改变根标签的一切的根节点

--- src/mypack/basic_components/View.tsx
+++ src/mypack/basic_components/View.tsx
@@ -4,15 +4,21 @@ import { ClassValue } from 'classnames/types'
 
 function View({
   className,
+  use = 'div',
   ...restProps
 }: Omit<JSX.IntrinsicElements['div'], 'className'> & {
+  /**
+   * 覆盖原生的className
+   */
   className?: ClassValue
-}) {
-  return (
-    <div
-      className={classnames(className)}
-      {...restProps}
-    />
-  )
+  /**
+   * 表示渲染所使用的标签，默认使用DIV
+   */
+  use?: 'div' | 'section'
+}): JSX.Element {
+  return React.createElement(use, {
+    className: classnames(className),
+    ...restProps,
+  })
 }
 export default React.memo(View) as typeof View
