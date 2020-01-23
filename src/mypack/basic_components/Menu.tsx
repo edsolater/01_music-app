a875f00a9257cc39582ccee7009dedc12f1ad369
edsol <edsolater@qq.com>
2020-01-23 22:42:42 +0800

    补注释

--- src/mypack/basic_components/Menu.tsx
+++ src/mypack/basic_components/Menu.tsx
@@ -5,7 +5,7 @@ import { useMaster } from 'mypack/basic_components/customHooks'
 import { ComponentRoot, Slot } from '.'
 /**
  * TODO: Menu会引起Typescript的类型推断崩溃，肯定有什么问题（答：类型继承用得太过复杂）
- * TODO：这个Menu组件的内聚性打散了，必须重写
+ * TODO：这个Menu组件的内聚性打散了，太过复杂，必须重写
  */
 type MenuItemInfo = {
   itemIndex: number
