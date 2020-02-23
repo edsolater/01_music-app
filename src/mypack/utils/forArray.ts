1089d9cad625f9aaec2e72589bf16b59ca979025
edsolater <edsolater@qq.com>
2020-02-23 10:12:27 +0800

    开发（util——forArray）： 新增 createRange 方法

--- src/mypack/utils/forArray.ts
+++ src/mypack/utils/forArray.ts
@@ -1,26 +1,31 @@
-export function isLastIndex(array: unknown[], index: number) {
-  return array.length - 1 === index
-}
-export function isLastItem(array: unknown[], item: unknown) {
-  return array[array.length - 1] === item
-}
-export function isLast(array: unknown[], itemOrIndex: unknown) {
+export const isLastIndex = (array: unknown[], index: number) => array.length - 1 === index
+export const isLastItem = (array: unknown[], item: unknown) => array[array.length - 1] === item
+export const isFirstIndex = (array: /* 为了对称性 */ unknown[], index: number) => 0 === index
+export const isFirstItem = (array: unknown[], item: unknown) => array[0] === item
+
+export const isLast = (array: unknown[], itemOrIndex: unknown) => {
   if (typeof itemOrIndex === 'number') {
     return isLastIndex(array, itemOrIndex)
   } else {
     return isLastItem(array, itemOrIndex)
   }
 }
-export function isFirstIndex(array/* 为了对称性 */: unknown[], index: number) {
-  return 0 === index
-}
-export function isFirstItem(array: unknown[], item: unknown) {
-  return array[0] === item
-}
-export function isFirst(array: unknown[], itemOrIndex: unknown) {
+export const isFirst = (array: unknown[], itemOrIndex: unknown) => {
   if (typeof itemOrIndex === 'number') {
     return isFirstIndex(array, itemOrIndex)
   } else {
     return isFirstItem(array, itemOrIndex)
   }
 }
+
+export const createArray = (config: { repeating?: unknown; count?: number } = {}) =>
+  Array(config.count).fill(config.repeating)
+
+export const createRange = (
+  config: { min?: number; max?: number; count?: number; includeEnd?: boolean } = {},
+) => {
+  const min = config.min ?? (config.max ?? 10) - (config.count ?? 10)
+  const max = config.max ?? min + (config.count ?? 10)
+  return Array.from({ length: max - min + (config.includeEnd ? 1 : 0) }, (_, i) => i + min)
+}
+
