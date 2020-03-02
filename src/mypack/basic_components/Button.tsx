2fbc653191d575a43d0237db72ada931058ee197
edsolater <edsolater@qq.com>
2020-03-03 00:05:10 +0800

    删除（Button组件）：去除Button的 _isOn _isOff 属性

--- src/mypack/basic_components/Button.tsx
+++ src/mypack/basic_components/Button.tsx
@@ -1,30 +1,17 @@
 import React, { ComponentProps } from 'react'
 import './Button.scss'
-import { View,  } from '.'
+import { View } from '.'
 
 //TODO： 常规操作：定义各种按钮样式
 function Button(
   props: ComponentProps<typeof View> & {
-    /**
-     * 按钮关闭
-     */
-    isOn?: boolean
-    /**
-     * 按钮打开
-     */
-    isOff?: boolean
     /**
      * 按钮处于失效状态
      */
     disabled?: boolean
   },
 ) {
-  return (
-    <View
-      {...props}
-      _componentName_={['Button', { _disabled: props.disabled, _on: props.isOn, _off: props.isOff }]}
-    ></View>
-  )
+  return <View {...props} _componentName_={['Button', { _disabled: props.disabled }]}></View>
 }
 
 export default React.memo(Button) as typeof Button
