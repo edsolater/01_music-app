125bbfbe3975e2d1c7e7f7908bc472adcc48f4ed
edsolater <edsolater@qq.com>
2020-03-02 23:24:44 +0800

    补：去除Intro、Block

--- src/components/MainAppContent.tsx
+++ src/components/MainAppContent.tsx
@@ -5,7 +5,6 @@ import {
   Icon,
   Text,
   Group,
-  Block,
   ImageBox,
   Button,
   List,
@@ -23,13 +22,13 @@ export default function MainAppContent() {
     <View className='main-app-content'>
       <Text headline>歌单</Text>
       <Grid className='collection-info'>
-        <Block className='thumbnail'>
+        <View className='thumbnail'>
           <ImageBox src={appData.collectionInfo.thumbnail} className='thumbnail-pic' />
           {/* TODO: 以后SVGIcon要动态的化不能简单地作为 img 处理 */}
           <Icon src={heartIcon} className='thumbnail-icon' />
-        </Block>
+        </View>
         <Text largeTitle>{appData.collectionInfo.title}</Text>
-        <Block className='creator'>
+        <View className='creator'>
           <Avatar src={appData.collectionInfo.creatorInfo.avatar} className='creator-avatar' />
           <Text subhead className='creator-nickname'>
             {appData.collectionInfo.creatorInfo.nickName}
@@ -37,7 +36,7 @@ export default function MainAppContent() {
           <Text footnote className='creator-create-time'>
             {appData.collectionInfo.createTime} 创建
           </Text>
-        </Block>
+        </View>
         <Group className='buttons'>
           <Button>收藏</Button>
           <Button>评论</Button>
