d0167cb09b1eaf6af47bd3be25fab95adcec12a3
zhanggongying <edsolater@qq.com>
2020-03-04 00:41:46 +0800

    小修改（Audio）： Audio也基于View之上

--- src/mypack/components/Audio.tsx
+++ src/mypack/components/Audio.tsx
@@ -1,5 +1,20 @@
-import React from 'react'
+import React, { ComponentProps } from 'react'
+import { View } from '.'
 
-export default function Audio(props: JSX.IntrinsicElements['audio']) {
-  return <audio {...props} />
+type IProps = ComponentProps<typeof View> & {
+  /**
+   * 除className,style,onClick外的原生属性的
+   */
+  html?: JSX.IntrinsicElements['audio']
+  /**
+   * 特意提取出一个img的常用属性
+   */
+  src?: JSX.IntrinsicElements['audio']['src']
 }
+
+const Audio = React.forwardRef((props: IProps, ref: any) => (
+  <View ref={ref} $tag='audio' html={{ src: props.src, ...props.html }}>
+    {props.children}
+  </View>
+))
+export default React.memo(Audio) as typeof Audio
