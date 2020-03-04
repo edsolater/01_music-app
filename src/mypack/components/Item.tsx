75504111aaae8b38ae033314d4566aad0c37c469
zhanggongying <edsolater@qq.com>
2020-03-04 21:30:02 +0800

    小修改

--- src/mypack/components/Item.tsx
+++ src/mypack/components/Item.tsx
@@ -4,13 +4,8 @@ import { View } from '.'
 /**
  * 用于横向布局
  */
-function Item(
-  props: ComponentProps<typeof View> & {
-    hello?: string
-  },
-  ref: any,
-) {
-  return <View ref={ref} {...props} className={[props.className, 'Item']} />
+function Item(props: ComponentProps<typeof View>, ref: any) {
+  return <View ref={ref} {...props} $componentName='Item' />
 }
 
 export default React.memo(React.forwardRef(Item)) as typeof Item
