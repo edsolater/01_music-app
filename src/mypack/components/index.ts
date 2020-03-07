947394b14a13bf9c6c695ab9f8a1b9cdb30d3090
zhanggongying <edsolater@qq.com>
2020-03-07 18:36:00 +0800

    补

--- src/mypack/components/index.ts
+++ src/mypack/components/index.ts
@@ -1,21 +1,20 @@
-export { default as Slot } from './lowLevel/Slot'
-export * from './lowLevel/Slot'
-export { default as View } from './lowLevel/View'
-export * from './lowLevel/View'
-export { default as Text } from './lowLevel/Text'
-export { default as Image } from './lowLevel/Image'
-export { default as Box } from './layoutWrapper/Box'
-export { default as Avatar } from './mediumLevel/Avatar'
-export { default as Icon } from './mediumLevel/Icon'
-export { default as Button } from './mediumLevel/Button'
-export { default as Slider } from './mediumLevel/Slider'
-export { default as Popover } from './mediumLevel/Popover'
-export { default as Badge } from './mediumLevel/Badge'
-export { default as Menu } from './highLevel/Menu'
-export { default as List } from './highLevel/List'
-export { default as ImageBox } from './highLevel/ImageBox'
-
 /**
  * 特殊组件
  */
 export * from './customHooks'
+/**
+ * 低级组件（实现的基础）
+ */
+export * from './lowLevel'
+/**
+ * 中等组件（低级组件的语义封装）
+ */
+export * from './mediumLevel'
+/**
+ * 高等组件（由中等组件组合）
+ */
+export * from './highLevel'
+/**
+ * 排版组件（页面的“盒子”元素）
+ */
+export * from './layoutWrapper'
