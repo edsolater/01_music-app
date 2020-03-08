10020ec9f1e4bce757048e8a42ee57a0a37b11de
zhanggongying <edsolater@qq.com>
2020-03-08 12:03:01 +0800

    补

--- src/mypack/components/index.ts
+++ src/mypack/components/index.ts
@@ -5,15 +5,15 @@ export * from './customHooks'
 /**
  * 低级组件（实现的基础）
  */
-export * from './lowLevel'
+export * from './basic'
 /**
  * 中等组件（低级组件的语义封装）
  */
-export * from './mediumLevel'
+export * from './medium'
 /**
  * 高等组件（由中等组件组合）
  */
-export * from './highLevel'
+export * from './complicated'
 /**
  * 排版组件（页面的“盒子”元素）
  */
