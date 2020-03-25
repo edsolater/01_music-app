187ce3fc51aef33b87772760ed83d0de152b3391
zhanggongying <edsolater@qq.com>
2020-03-25 23:28:03 +0800

    删掉（<App>）：删掉一层冗余的View

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,15 @@ import './App.scss'
 import { AlbumMenu, MainAppContent, AudioPlayer } from 'components'
 import { Provider } from 'react-redux'
 import { store } from 'store'
-import { View } from 'mypack/components/wrappers'
 
 function App() {
   return (
     <Provider store={store}>
-      <View className='app' as='main'>
-        <AlbumMenu />
-        <MainAppContent />
-        <AudioPlayer />
-      </View>
+      <AlbumMenu />
+      <MainAppContent />
+      <AudioPlayer />
     </Provider>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('app'))
