5b53ca2cb5f0455a354a4f45286f7143a49eb5d4
zhanggongying <edsolater@qq.com>
2020-03-28 01:03:08 +0800

    突发奇想（hooks）：新增useQueue，可用于保存路径

---
+++ src/mypack/components/customHooks/useQueue.ts
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+//TODO! 还没开始写
+/**
+ * 状态hooks
+ * 保存队列式（数组式）数据
+ * @example
+ *   const [path, pathSetter] = useItemQueue(['parent', 'child'])
+ * @param init 规定初始值
+ * @param onChangeCallback 当值改变时的回调
+ */
+export default function useQueue(
+  /**初始值，默认或不能解析时均为0 */
+  init?: unknown,
+  /** 值改变回调函数 */
+  onChangeCallback?: (newValue: number, oldValue: number) => any,
+) {
+  // 使用原生的useState
+  const [_numberState, _setNumber] = useState(Number(init) || 0)
+
+  //强化型管理器
+  const stateManager = {
+    /**原来数值的基础上做加减法 */
+    add: (deltaNumber: number | ((oldValue: number) => number)) =>
+      typeof deltaNumber === 'function'
+        ? stateManager.set(deltaNumber)
+        : stateManager.set((oldValue: number) => oldValue + deltaNumber),
+
+    /**自定义本react钩子函数的dispatche */
+    set: (newNumber: number | ((oldValue: number) => number)) => {
+      const newValue = typeof newNumber === 'function' ? newNumber(_numberState) : newNumber
+      onChangeCallback?.(newValue, _numberState)
+      _setNumber(newValue)
+    },
+  }
+  //返回特化过的（强化版）useState
+  return [_numberState, stateManager] as const
+}
