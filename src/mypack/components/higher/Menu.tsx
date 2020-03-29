a7a37c5e2341567bd63b55c67c9bae662a5b59b2
zhanggongying <edsolater@qq.com>
2020-03-29 17:45:23 +0800

    改（<Menu>）：类型声明更generic

--- src/mypack/components/higher/Menu.tsx
+++ src/mypack/components/higher/Menu.tsx
@@ -1,33 +1,25 @@
 import React, { ReactNode, ComponentProps } from 'react'
 
-import { Path } from 'mypack/class/StateStringPath'
-import { IconfontName } from 'iconfont/namelist'
 import { View, Slot } from '../wrappers'
 import { List } from '.'
 import { useQueue } from '../customHooks'
 
-type ItemInfo = {
-  //TODO: 这个label也要去掉，这样使用<Menu>组件才全无后顾之忧。最好能只传来一个id
-  label: string
-  icon?: IconfontName
-  subtitle?: string
-  index?: number
-  hasSomethingNew?: boolean
-  siblings?: GroupInfo['children']
-  group?: GroupInfo
-  detail?: unknown
+type ItemInfo<T> = T & {
+  index: number
+  siblings: T[]
+  group: GroupInfo<T>
 }
-type GroupInfo = {
+type GroupInfo<T> = {
   label: string
-  index?: number
-  children?: ItemInfo[]
-  path?: Path
+  index: number
+  children: T[]
+  path: [GroupInfo<T>, ItemInfo<T>]
 }
 
 /**
  * TODO： 把不是那么一眼扫过去就明白的逻辑都提出来
  */
-export default function Menu<T extends ItemInfo>(
+export default function Menu<T extends { label: string }>(
   props: ComponentProps<typeof View> & {
     //TODO: generic掉data中的ItemInfo的类型
     /**
@@ -39,23 +31,26 @@ export default function Menu<T extends ItemInfo>(
     /**
      * 选择某个菜单项时发起的回调
      */
-    onSelectItem?: (itemInfo: T, event: React.MouseEvent) => void
+    onSelectItem?: (itemInfo: ItemInfo<T>, event: React.MouseEvent) => void
     /**
      * 菜单项
      */
-    rendeItem?: (itemInfo: T, index: number) => ReactNode
+    rendeItem?: (itemInfo: ItemInfo<T>, index: number) => ReactNode
     /**
      * 菜单组的标头
      */
-    renderMenuGroup?: (groupInfo: GroupInfo, index: number) => ReactNode
+    renderMenuGroup?: (groupInfo: GroupInfo<T>, index: number) => ReactNode
   },
 ) {
-  const [currentMenuPath, pathSetters] = useQueue([{ label: '' }, { label: '' }] as [GroupInfo, T])
+  const [currentMenuPath, pathSetters] = useQueue([{ label: '' }, { label: '' }] as [
+    GroupInfo<T>,
+    ItemInfo<T>,
+  ])
   return (
     <View {...props} $componentName='Menu'>
       {props.children}
       {Object.entries(props.data).map(([groupName, groupItems], groupIndex) => {
-        const groupInfo: GroupInfo = {
+        const groupInfo = {
           label: groupName,
           index: groupIndex,
           children: groupItems,
@@ -73,7 +68,7 @@ export default function Menu<T extends ItemInfo>(
               data={groupItems}
               itemKey={(item) => item.label}
               renderItem={(menuItem, itemIndex) => {
-                const itemInfo: T = {
+                const itemInfo = {
                   ...menuItem,
                   group: groupInfo,
                   index: itemIndex,
