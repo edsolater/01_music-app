b93c5533938bc5f6044956de38e1c247547daf6f
zhanggongying <edsolater@qq.com>
2020-03-29 17:50:57 +0800

    整理：彻底删除useMaster系

--- src/mypack/class/index.ts
+++ src/mypack/class/index.ts
@@ -1,5 +1,1 @@
-export { default as StateBoolean } from './StateBoolean'
-export { default as StateNumber } from './StateNumber'
-export { default as StateStringPath } from './StateStringPath'
 export { default as Time } from './Time'
-export { default as PoolMarket } from './PoolMarket'
--- src/mypack/components/customHooks/useMasterBoolean.ts
+++
@@ -1,5 +0,0 @@
-import useMaster from './useMaster'
-
-const useMasterBoolean = (initValue: boolean) =>
-  useMaster({ type: 'boolean', init: initValue ?? false })
-export default useMasterBoolean
