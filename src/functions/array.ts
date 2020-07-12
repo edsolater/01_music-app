678cc717c699068e2267f9fcf6f51edda82e987b
edsolater <edsolater@qq.com>
2020-07-12 16:15:30 +0800

    【functions文件】新增createArray 函数

--- src/functions/array.ts
+++ src/functions/array.ts
@@ -1,10 +1,35 @@
-export function insert<T extends any, K extends any>(target: T[], toInsert: K): (T | K)[] {
-  const copyed = [...target]
+/**
+ * 浅拷贝数组，在数组的每两项间的空隙，插入值
+ * @param target 目标数组
+ * @param toInsert 中间插入的值
+ * @example
+ * fillArrayGap(['hello','world','!'],1) => ['hello', 1, 'world', 1, '!']
+ */
+export function fillGap<T, K>(target: T[], toInsert: K): (T | K)[] {
+  const copyed: (T | K)[] = []
   for (let i = 0; i < target.length - 1; i++) {
-    // @ts-expect-error
-    copyed.splice(2 * i + 1, 0, toInsert)
+    copyed.push(target[i], toInsert)
   }
+  copyed.pop()
   return copyed
 }
 
+/**
+ * 创造有初始长度的数组，默认空值为undefined
+ * @param length 数组长度
+ * @param fill 空位置上的值，默认undefined
+ * @example
+ * createArray(3) => [undefined, undefined, undefined]
+ * createArray(3, 'ha') => ['ha', 'ha', 'ha']
+ * createArray(3, i => i) => [0, 1, 2]
+ */
+export function createArray<T = undefined>(length: number, fill?: T | ((idx: number) => T)): T[] {
+  if (typeof fill === 'function') {
+    //@ts-ignore
+    return Array.from({ length }, (_, i) => fill(i))
+  } else {
+    //@ts-ignore
+    return Array.from({ length }, () => fill)
+  }
+}
 // IDEA 批处理，类似 listDo(target, action1, action2) => newList
